0960c810e443aba57252d0240cad1405f281eb02
MrCOTst <mypostgg@gmail.com>
2022-08-28 11:45:00 +0300

    2nd task is over

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const cardSet = document.querySelector(".gallery");
+const cardsMarkup = createImageCards(galleryItems);
+
+cardSet.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createImageCards(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+};
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+} );
